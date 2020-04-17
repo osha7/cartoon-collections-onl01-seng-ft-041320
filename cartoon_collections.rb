2b09afc656e5a19ccb728158f3bb109016acbea2
Osha G <ogroetz@gmail.com>
2020-04-17 12:59:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = []
   planeteer_calls.map { |calls| calls.upcase }
-  planeteer_calls.each { |name| puts "#{name}"}
+  planeteer_calls.each { |name| puts "#{name}!"}
  
 end
 
