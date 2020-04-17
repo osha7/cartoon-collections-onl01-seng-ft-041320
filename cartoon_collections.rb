e5a101dea59844ea5c5a8985e5ccd40c68f7ae39
Osha G <ogroetz@gmail.com>
2020-04-17 12:38:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+ dwarf_names.each_with_index { |name, index| puts "#{index+1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+ planeteer_calls.each { |name| puts "#{name}"}
+ 
+ #my_collect(collection) do |lang|
+  lang.upcase
+end
 end
 
 def long_planeteer_calls# code an argument here
