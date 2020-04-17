9b4388d78a1dd8bc0cb084d06f136e89016e471b
Osha G <ogroetz@gmail.com>
2020-04-17 13:46:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect{|i| i = cheese_types}
-  #array.include?(cheese_types)
+  array.detect{|i| i == cheese_types}
 end
